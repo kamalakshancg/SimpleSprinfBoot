81697a0f98637f3ade8e0e46b934d5ab0572bb49
kamalakshaNC <47631709+kamalakshancg@users.noreply.github.com>
2022-03-05 21:22:40 +0530

    Updated

--- src/main/java/com/example/SpringBootLog4j/SpringBootLog4jApplication.java
+++ src/main/java/com/example/SpringBootLog4j/SpringBootLog4jApplication.java
@@ -13,7 +13,7 @@ public class SpringBootLog4jApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootLog4jApplication.class, args);
-		int i = 20;
+		int i = 10;
 		
 		
 		
